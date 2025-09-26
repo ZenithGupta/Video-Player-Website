ba297283f939c4084e0319d28a688de07e601e5c
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2025-09-26 22:35:44 +0530

    Frontend deploy (#20)
    * Corrected the workflow typo to workflows.
    
    * Updated deploy.yml
    
    * Added permissions to create new branch in deploy.yml.
    
    * Updated from BrowserRouter to HashRouter.
    
    * Reverted back to BrowserRouter.
    
    * Corrected website naming in configs.
    
    * Updating deployment to main branch.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with BrowserRouter */}
+    <BrowserRouter basename="/Video-Player-Website"> {/* Wrap App with BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
