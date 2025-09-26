e0402394fc4dd0d90370b09892036414ef392a0c
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2025-09-26 22:24:25 +0530

    Corrected website naming in configs.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/video-player-website"> {/* Wrap App with BrowserRouter */}
+    <BrowserRouter basename="/Video-Player-Website"> {/* Wrap App with BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
