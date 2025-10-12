963c7fc86f1955f9a6562ef3ac4d90894bcd2753
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2025-10-12 08:11:14 +0530

    Added Prod API Route and also updated deploy script to set NODE_ENV.

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 // Create a new file: frontend/src/config/api.js
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'http://your-production-domain.com/api'  // Replace with your production URL
+  ? 'http://34.160.235.179/api'  // Replace with your production URL
   : 'http://0.0.0.0:8000/api';  // Use Docker service name for development
 
 export default API_URL;
