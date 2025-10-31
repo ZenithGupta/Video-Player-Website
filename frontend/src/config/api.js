01b9f74bdb96260c4735076880bdcb1a3282eef6
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2025-10-31 21:07:27 +0530

    Added correct backend api endpoint in frontend.

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
 // Create a new file: frontend/src/config/api.js
-const API_URL = 'https://onelastmove.com/api';  // Use Docker service name for development
+const API_URL = 'http://onelastmove.com/api';  // Use Docker service name for development
 
 export default API_URL;
